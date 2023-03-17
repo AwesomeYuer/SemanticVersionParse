075ffd91cc607c11bc9776fb68dc1537f6b0e541
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-17 23:36:21 +0800

    v => (SemanticVersion) NuGetVersion.Parse(v)

--- SemanticVersionParse/Program.cs
+++ SemanticVersionParse/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NuGet.Versioning;
 using Semver;
+using System.Security.Cryptography.X509Certificates;
 
 Console.WriteLine($"using {nameof(NuGet)}.{nameof(NuGet.Versioning)}.{nameof(SemanticVersion)} and {nameof(Semver)}.{nameof(SemVersion)}");
 
@@ -15,13 +16,17 @@ var versions = new string[]
     , "1.0.4-ztm.8"
     , "1.0.4-ztm"
     , "1.0.4-Atm4"
-    , "1.0.4-Atm"
+    , "1.0.004-Atm"
     , "1.0.4-pre-release"
     , "1.0.4-preview3.4.243"
     , "1.0.4-preview2.4.243"
     , "1.0.4-beta.4.243"
     , "1.0.1-alpha.4.243"
     , "1.0.1"
+    //, "1.0.1.900"
+    , "1.01"
+    , "1.001.01"
+
 };
 
 versions
@@ -55,16 +60,24 @@ versions
         (
             (i, v) =>
             {
-                var textEqual = v.v1.ToFullString () == v.v2.ToString ();
+                var v1 = v.v1;
+                var v2 = v.v2;
+                var textEqual = v1.ToFullString () == v2.ToString ();
+
+                Console.WriteLine($"{v1.GetType().Name}: {v1.Release}, {v1.IsPrerelease}" );
+                Console.WriteLine($"{v2.GetType().Name}: {v2.Prerelease}, {v2.IsPrerelease}");
+
                 if (!textEqual)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
-                Console.WriteLine($@"({i}): {nameof(SemanticVersion)}:""{v.v1}"" {(textEqual ? "==" : "!=")} {nameof(SemVersion)}:""{v.v2}""");
+                Console.WriteLine($@"({i}): {v1.GetType().Name}:""{v1}"" {(textEqual ? "==" : "!=")} {v2.GetType().Name}:""{v2}""");
                 if (!textEqual)
                 {
                     Console.ResetColor();
                 }
+
+                Console.WriteLine($"===================================");
                 return false;
             }
         )
